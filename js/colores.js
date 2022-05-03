a1e156940b91d1f3562e6478ffbe47e4ed161093
belcar-ceci <carbel.cecilia@gmail.com>
2022-05-03 12:40:49 +0200

    adding variable and selectores

--- js/colores.js
+++ js/colores.js
@@ -1,1 +1,11 @@
 "use strict";
+//variables que llaman a selectores
+const circle = document.querySelectorAll(".circle");
+const codigoRgb = document.querySelectorAll(".color_rgb");
+const colors = randomColor(3);
+const selectColor = colors[Math.floor(Math.random() * 3)];
+codigoRgb.textContent = selectColor;
+
+const circleCount = 3;
+
+
