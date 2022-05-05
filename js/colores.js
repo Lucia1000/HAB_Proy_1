9fcf5fa84077aa61b2c688e3b75be1d1a661870c
belcar-ceci <carbel.cecilia@gmail.com>
2022-05-05 03:14:24 +0200

    adding css 3d circle

--- js/colores.js
+++ js/colores.js
@@ -1,15 +1,18 @@
 "use strict";
 //Contador de aciertos o errores
-let aciertos = 0;
-let errores = 0
+
 
 //variables que llaman a selectores
 const circles = document.querySelectorAll(".circle"); //variable que llame a la clase de los circulos
-const codigoRgb = document.querySelectorAll("#valorRgb"); //variable para mostrar los 3 valores de RGB
-const resultado = document.querySelectorAll("#resultado"); //variable que llama a los aciertos o errores
-const reiniciar = document.querySelectorAll("#reset");// variable que reincia el juego una vez que pierdas o ganes
-const contadorAciertos = document.querySelectorAll("#acierto"); //variables que mostrara los aciertos
-const contadorErrores = document.querySelectorAll("#error"); //variable que mostrara los errores
+const codigoRgb = document.getElementById("valorRgb"); //variable para mostrar los 3 valores de RGB
+const resultado = document.getElementById("resultado"); //variable que llama a los aciertos o errores
+const reiniciar = document.getElementById("reset");// variable que reincia el juego una vez que pierdas o ganes
+const contadorAciertos = document.getElementById("acierto"); //variables que mostrara los aciertos
+const contadorErrores = document.getElementById("error"); //variable que mostrara los errores
+
+let aciertos = 0;
+let errores = 0
+console.log(errores);
 
 
 //Random logica
@@ -43,5 +46,55 @@ const valorRandomColor = () => {
 
 /*4. Pintar color a circulos */
 const printCircle = () => {
-
+  const colorsCircles = Math.floor(Math.random() * 3);
+  for (let i = 0; i < circles.length; i++) {
+    if (i === colorsCircles) {
+      circles[i].style.backgroundColor = codigoRgb.innerHTML
+    } else {
+      circles[i].style.backgroundColor = valorRandomColor();
+    }
+  }
+  //console.log(colorsCircles())
 }
+/*5. seleccionar color ganador */
+const pickColor = (event) => {
+  const colorGanador = event.target; //devuelve el elemento DOM que desencadenó un evento específico, por lo que podemos recuperar cualquier propiedad/atributo que tenga un valor
+  if (resultado.innerHTML === 'Ganaste') {
+    alerta("cambia color");
+  } else if (
+    colorGanador.style.backgroundColor = codigoRgb.innerHTML.toLowerCase()
+  ) {
+    resultado.innerHTML = 'Ganaste';
+    aciertos++;
+    contadorAciertos.innerHTML = aciertos;
+    //se agrega funcion de inicio de juego que da colores y valores nuevos
+    startGame();
+  } else {
+    resultado.innerHTML = 'Incorrecto, prueba de nuevo';
+    errores++;
+    contadorErrores.innerHTML = errores;
+  if (errores === 3) {
+      respuesta.innerHTML = 'Lo siento, perdiste';//sepinta html
+
+      alert('perdiste');
+    }
+  }
+}
+const clickCircle = () => {
+  for (let i = 0; i < circles.length; i++) {
+    circles[i].addEventListener('click', pickColor);
+  }
+}
+
+const startGame = () => {
+  if (aciertos === 3) {
+    resultado.innerHTML = 'Ganaste!';//se pinta en html
+    
+    alert("ganaste")
+  }
+  codigoRgb.innerHTML = randomColorRGB().toUpperCase();
+  clickCircle();
+  printCircle();
+  resultado.innerHTML = 'Elige un Color';
+}
+startGame();
