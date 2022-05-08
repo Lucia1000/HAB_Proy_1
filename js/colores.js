00082fda5bcf1cee82c66a4b15fac3c60da7cff9
Lucia Perez <mellamolucia@gmail.com>
2022-05-09 00:36:18 +0200

    faltan nuevas cajas

--- js/colores.js
+++ js/colores.js
@@ -6,8 +6,9 @@ const l2 = document.querySelector("#circulo2");
 const l3 = document.querySelector("#circulo3");
 
 const ul = document.querySelector("ul");
-const acierto = document.querySelector(".aciertos");
-const error = document.querySelector(".errores");
+let acierto = document.querySelector(".aciertos");
+let error = document.querySelector(".errores");
+const resultado = document.querySelector("#resultado");
 
 //Genero numeros aleatorios de 0 a 255 (para el rgb)
 const getRandomNum = (max) => {
@@ -19,47 +20,100 @@ const getRandomRGB = () => {
   return [getRandomNum(255), getRandomNum(255), getRandomNum(255)];
 };
 
-//al recargar la página genera colores de los li y un nuevo rgb
-
+//Genera colores de los li de colores parecidos
 const firstColor = getRandomRGB();
 
-const secondColor = [firstColor[0] - 80, firstColor[1], firstColor[2] + 50];
+const secondColor = [firstColor[0] - 20, firstColor[1], firstColor[2] + 40];
 
 const thirdColor = [firstColor[0], firstColor[1] + 50, firstColor[2] - 20];
+
 //paso el array obtenido a string
 const rgbArrayToString = (rgbArray) => {
   return `rgb(${rgbArray[0]}, ${rgbArray[1]}, ${rgbArray[2]})`;
 };
 
+//asigno a cada circulo un color parecido al rgb
 l1.style.backgroundColor = rgbArrayToString(firstColor);
 l2.style.backgroundColor = rgbArrayToString(secondColor);
 l3.style.backgroundColor = rgbArrayToString(thirdColor);
 
+//array de 3 numeros, con los colores generados
 const array = [firstColor, secondColor, thirdColor];
 
-// -TENGO Q MOVER LOS l1, L2 Y L3 PARA Q NO SE SEPA CUAL ES
-
+//el color correcto es (aleatoriamente) uno de los tres colores generados
 const colorCorrecto = array[getRandomNum(2)];
-console.log(colorCorrecto);
+// console.log(colorCorrecto);
+
+//El color correcto es igual al codigo rgb
 codigoRgb.textContent = rgbArrayToString(colorCorrecto);
 
-//Si click en el l1 , sumo un acierto, si no resto 1 (HACER CONTADOR)...)
-
-// for (let li = 0; li < 3; li++) {
+// Si click en el correcto +1 a los aciertos, sino +1 en los errores
 ul.addEventListener("click", (event) => {
-  console.log(
-    rgbArrayToString(colorCorrecto),
-    event.target.style.backgroundColor
-  );
+  // console.log(rgbArrayToString(colorCorrecto), event.target.style.backgroundColor);
   if (rgbArrayToString(colorCorrecto) === event.target.style.backgroundColor) {
     acierto.textContent = +acierto.textContent + 1;
+
+    codigoRgb.textContent = getRandomRGB();
+
+    if (+acierto === 3) {
+      resultado.textContent = "Ganaste!!!";
+    }
   } else {
     error.textContent = +error.textContent + 1;
+    codigoRgb.textContent = getRandomRGB();
+
+    if (+error === 3) {
+      resultado.textContent = "Perdiste!!";
+    }
   }
 });
 
-// if (acierto === 3) {
-//   alert("GANADOR!!!");
-// } else if (error === 3) {
-//   alert("PERDISTE. INTÉNTALO OTRA VEZ!!!");
-// }
+// li.addEventListener("click", (event) => {
+//   // console.log(rgbArrayToString(colorCorrecto),event.target.style.backgroundColor );
+
+//   rgbArrayToString(colorCorrecto) === event.target.textContent;
+// });
+
+//*5. seleccionar color ganador y se realiza el contador de aciertos y errores*/
+// const pickColor = (e) => {
+//   const colorCorrecto = e.target; //devuelve el elemento DOM que desencadenó un evento específico, por lo que podemos recuperar cualquier propiedad/atributo que tenga un valor
+//   if (resultado.textContent === "Ganaste") {
+//     alert("cambia color");
+//   } else if (colorCorrecto.style.backgroundColor === codigoRgb.textContent) {
+//     resultado.textContent = "Ganaste";
+//     aciertos++;
+//     acierto.textContent = aciertos;
+//     //se agrega funcion de inicio de juego que da colores y valores nuevos
+//     startGame();
+//   } else {
+//     resultado.textContent = "Incorrecto, prueba de nuevo";
+//     errores++;
+//     error.textContent = errores;
+//     console.log(errores);
+//     if (errores === 3) {
+//       resultado.textContent = "Lo siento, perdiste"; //sepinta html
+//       //alert('perdiste');
+//     }
+//   }
+// };
+
+// const startGame = () => {
+//   if (+acierto.textContent === 3) {
+//     resultado.textContent = "Ganaste!"; //se pinta en html
+//     //alert("ganaste")
+//   }
+//   codigoRgb.textContent = rgbArrayToString(colorCorrecto);
+
+//   resultado.textContent = "Elige un Color";
+// };
+// startGame();
+
+// const starGame = () => {
+//   if (+acierto.textContent === 3) {
+//     alert("HAS GANADO!!!");
+//   } else (+error.textContent === 3) {
+//     alert("PERDISTE. INTÉNTALO OTRA VEZ!!!");
+//   }
+// };
+
+// starGame();
